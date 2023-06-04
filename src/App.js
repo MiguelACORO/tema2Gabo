d6d3c389e7a21130147a6b71771aaebae3224218
Miguel Alfonso Córdoba Rojas <18470271@campeche.tecnm.mx>
2023-06-03 18:07:09 -0600

    'finished'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	let [color, setColor] = useState('#FFFFFF')
+	let [count, setCount] = useState(0)
+
+	return (
+		<div className="main">
+			<h4>Counter App</h4>
+			<div className="colors">
+				<button
+					onClick={() => setColor('#ffffff')}
+					style={{ backgroundColor: '#ffffff' }}
+				></button>
+				<button
+					onClick={() => setColor('#ff0000')}
+					style={{ backgroundColor: '#ff0000' }}
+				></button>
+				<button
+					onClick={() => setColor('#008000')}
+					style={{ backgroundColor: '#008000' }}
+				></button>
+				<button
+					onClick={() => setColor('#0000ff')}
+					style={{ backgroundColor: '#0000ff' }}
+				></button>
+				<button
+					onClick={() => setColor('#fbff00')}
+					style={{ backgroundColor: '#fbff00' }}
+				></button>
+			</div>
+			<div className="counter" style={{ border: `4px solid ${color}` }}>
+				<span className="count" style={{ color: `${color}` }}>
+					{count}
+				</span>
+				<div className="buttons">
+					<button onClick={() => setCount(count - 1)}>-</button>
+					<span onClick={() => setCount(0)}>Reset</span>
+					<button onClick={() => setCount(count + 1)}>+</button>
+				</div>
+			</div>
+		</div>
+	)
 }
-
-export default App;
